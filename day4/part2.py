91ee976b37f8e8e0befd3596b2503474e321e680
Agustín Covarrubias <agustin@agucova.dev>
2023-12-06 01:05:15 -0300

    feat(day4): part2

--- day4/part2.py
+++ day4/part2.py
@@ -4,15 +4,22 @@ import re
 INPUT_FILE = Path(__file__).parent.parent / "inputs" / "day4.inp"
 NUMBERS_REGEX = re.compile("\d+")
 
-points = []
 with open(INPUT_FILE) as f:
-    for line in f.readlines():
+    lines = f.readlines()
+    n_cards = int(NUMBERS_REGEX.findall(lines[-1].split(":")[0])[0])
+    card_counts = [1 for i in range(0, n_cards)]
+    for i, line in enumerate(lines):
+        if not line.strip():
+            continue
+
         winning_line, numbers_in_card = line.split("|")
         winning_line = winning_line.split(": ")[1]
         winning_numbers = len(
             set(NUMBERS_REGEX.findall(winning_line))
             & set(NUMBERS_REGEX.findall(numbers_in_card))
         )
-        points.append(2 ** (winning_numbers - 1) if winning_numbers >= 1 else 0)
+        for i_copied_card in range(i + 1, i + 1 + winning_numbers):
+            card_counts[i_copied_card] += card_counts[i]
 
-print(sum(points))
+
+print(sum(card_counts))
